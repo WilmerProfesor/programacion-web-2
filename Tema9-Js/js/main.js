febcf42ce08b5ef7f2a74dbffa0eb11e85222dff
WilmerProfesor <wilmerprofesor@gmail.com>
2024-09-21 09:45:43 -0500

    Sesion Js

---
+++ Tema9-Js/js/main.js
@@ -0,0 +1,20 @@
+let x,y;
+x=Number(prompt("X?"));
+
+if(x%2==0){
+    console.log("Par");    
+}else{
+    console.log("Impar");
+}
+
+
+// Leer un número desde el teclado y 
+// si el mismmo tiene 3 dígitos mostrar si es o no capicua
+
+
+// Los primeros n números enteros
+// flizz-Buzz
+// multiplo de 3 muestra el número y la palabra flizz
+// multiplo de 5 muestra el número y la palabra Buzz
+// multiplo de 3 y 5 muestra el número y la palabra flizz-Buzz
+
